1b5452a6d4d98633c2ce3fe1d8efb239aef1a1cf
Alonzo Pérez Cristhian <laespiga0210@gmail.com>
2025-12-04 11:05:54 -0500

    Actualización de hora

--- src/main/java/com/laespiga/laespigabackend/LaEspigabackendApplication.java
+++ src/main/java/com/laespiga/laespigabackend/LaEspigabackendApplication.java
@@ -1,8 +1,11 @@
 package com.laespiga.laespigabackend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling; // Importar
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling // Habilitar tareas programadas
@@ -10,5 +13,15 @@ public class LaEspigabackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LaEspigabackendApplication.class, args);
+    }
+
+    /**
+     * Este método se ejecuta una sola vez justo después de que la aplicación inicia.
+     * Establece la zona horaria por defecto de toda la JVM a America/Lima.
+     */
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
+        System.out.println("Zona horaria configurada a: " + new java.util.Date());
     }
 }
